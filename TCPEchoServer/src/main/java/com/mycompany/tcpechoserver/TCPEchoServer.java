e0ce642639a4e7da24cd789ad9d24a796ffb8f4b
tiphaine <23249064@studentmail.ul.ie>
2024-02-29 11:01:51 +0000

    logic of the client asking the server to display the schedule

--- TCPEchoServer/src/main/java/com/mycompany/tcpechoserver/TCPEchoServer.java
+++ TCPEchoServer/src/main/java/com/mycompany/tcpechoserver/TCPEchoServer.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package com.mycompany.tcpechoserver;
+import data.Classes;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,9 +17,11 @@ public class TCPEchoServer {
   private static ServerSocket servSock;
   private static final int PORT = 1234;
   private static int clientConnections = 0;
+  private static ArrayList<Classes> classes;
 
   public static void main(String[] args) {
     System.out.println("Opening port...\n");
+    classes = new ArrayList();
     try 
     {
         servSock = new ServerSocket(PORT);      //Step 1.
@@ -45,13 +49,14 @@ public class TCPEchoServer {
       BufferedReader in = new BufferedReader( new InputStreamReader(link.getInputStream())); //Step 3.
       PrintWriter out = new PrintWriter(link.getOutputStream(),true); //Step 3.
       
-      String message = in.readLine();         //Step 4.
-      System.out.println("Message received from client: " + clientConnections + "  "+ message);
+      String messageClient = in.readLine();         //Step 4.
+      System.out.println("Message received from client: " + clientConnections + "  "+ messageClient);
+      String[] actionClient = messageClient.split("/");
       
       String action;
       
       //Receiving the type of action wanted
-      switch(message){
+      switch(actionClient[0]){
           case "0":
               action = "add";
               break;
@@ -67,10 +72,41 @@ public class TCPEchoServer {
       }
       
       //Depending on the action coming from the client, do the thing
+      if(action == "consult"){
+          
+          //TEST
+          Classes classTest = new Classes("CS4076", Classes.DaysOfTheWeek.Monday, 9, 11, "CS2044");
+          classes.add(classTest);
+          //
+          
+          if(classes.size() == 0){
+              out.println("The schedule is empty ! Add a class first.");
+          } else if (actionClient.length < 1){
+              out.println("No class code found");
+          } else {
+            String classCode = actionClient[1];
+            String schedule = "CLASS";
+            for(int i = 0; i < classes.size(); i ++){
+                if(classes.get(i).getClassCode().equals(classCode)){
+                    schedule += "-" + classes.get(i).getClassCode() + "/"
+                            + classes.get(i).getRoom() + "/" +
+                            classes.get(i).getDay().toString() + "/" +
+                            classes.get(i).getStartingTime() + "/" +
+                            classes.get(i).getEndingTime();
+                }
+                System.out.println("Class " + classes.get(i).getClassCode() + 
+                        " in room " + classes.get(i).getRoom() + " starting at " +
+                        classes.get(i).getStartingTime() + " until " + classes.get(i).getEndingTime()
+                        + " on " + classes.get(i).getDay().toString());
+            }
+            if(schedule.equals("CLASS")){
+                schedule = "No class found with this code";
+            }
+            out.println(schedule);
+          }
+      }
       
-      // TODO: check if the exception has a correct behaviour
-      
-      out.println("Response from Server (Capitalized Message): " + message.toUpperCase());     //Step 4.
+      //out.println("Response from Server (Capitalized Message): " + message.toUpperCase());     //Step 4.
      }
     catch(IOException e)
     {
