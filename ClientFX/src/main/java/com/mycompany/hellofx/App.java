2b446c042a3b3bcaea5e149ecce854150e71a9c7
22345477Katie <150158819+22345477Katie@users.noreply.github.com>
2024-03-05 00:12:33 +0000

    Complete Functionality (not complete GUI)

--- ClientFX/src/main/java/com/mycompany/hellofx/App.java
+++ ClientFX/src/main/java/com/mycompany/hellofx/App.java
@@ -23,14 +23,17 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
 
@@ -41,8 +44,11 @@ public class App extends Application {
   static InetAddress host;
   static final int PORT = 1234;
   Label label = new Label("Response From Server Will Display Here");
+  Label title = new Label("Welcome to your class scheduler !");
   Label labelDisplay;
   Label classLabel;
+  Label labelDelete;
+  Label titleDisplay;
   TextField textFieldRoomDisplay;
   TextField moduleCode;
   ChoiceBox dayOfWeek;
@@ -50,18 +56,34 @@ public class App extends Application {
   ChoiceBox endTime;
   TextField roomCode;
   Button confirmAdd;
+  Button confirmRemove;
   String transmission;
   Button buttonAdd = new Button("Add class");
   Button buttonRemove = new Button("Remove class");
   Button buttonDisplay = new Button("Display schedule");
   Button confirmDisplay;
+  Button buttonQuit = new Button("STOP");
+  
 
     @Override
     public void start(Stage stage) {
 
-        
-        VBox box= new VBox( buttonAdd, buttonRemove, buttonDisplay, label);
+        title.setStyle("-fx-text-fill: #002633; -fx-font-weight: bold; "
+                    + "-fx-font-size: 20px");
+        buttonAdd.setStyle("-fx-background-color: #002633; -fx-text-fill: #ffffff; -fx-font-weight: bold");
+        buttonRemove.setStyle("-fx-background-color: #002633; -fx-text-fill: #ffffff;  -fx-font-weight: bold");
+        buttonDisplay.setStyle("-fx-background-color: #002633; -fx-text-fill: #ffffff;  -fx-font-weight: bold");
+        label.setStyle("-fx-text-style: italic");
+        VBox box= new VBox(title, buttonAdd, buttonRemove, buttonDisplay, buttonQuit /* label*/);
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(20);
         var scene = new Scene(box, 640, 480);
+        
+        Button buttonHome = new Button ("Back to home");
+        buttonHome.setStyle("-fx-background-color: #002633; -fx-text-fill: white;"
+                    + "-fx-border-color: white");
+        
+        
 
         buttonAdd.setOnAction(new EventHandler<ActionEvent>(){
         @Override
@@ -120,10 +142,12 @@ public class App extends Application {
                         
                         String[] startTimeSplit = startTime.getValue().toString().split(":");
                         String[] endTimeSplit = endTime.getValue().toString().split(":");
-                        String message = transmission = "0/"+ moduleCode.getText()+"/"+dayOfWeek.getValue()+"/"+startTimeSplit[0]+"/"+endTimeSplit[0]+"/"+roomCode.getText();
+                        String message = "0/"+ moduleCode.getText()+"/"+dayOfWeek.getValue()+"/"+startTimeSplit[0]+"/"+endTimeSplit[0]+"/"+roomCode.getText();
                         out.println(message);
                         response = in.readLine();
                         label.setText(response);
+                        
+                        
                         
                         
                         try
@@ -143,7 +167,19 @@ public class App extends Application {
                     {
                         
                     }
+                    Button buttonAddHome = new Button ("Back to home");
+            buttonAddHome.setOnAction(new EventHandler<ActionEvent> (){
+                            
+                @Override
+                public void handle(ActionEvent t){
+                    stage.setScene(scene);
+                }
+            });
+                    VBox box = new VBox(label, buttonAddHome);
+               var sceneConfirmAdd = new Scene(box, 640, 480);
+               stage.setScene (sceneConfirmAdd);
                }
+               
            });
             VBox box = new VBox(classLabel, moduleCode, dayOfWeek, startTime, endTime, roomCode,confirmAdd, buttonHome);
             var sceneAdd = new Scene(box, 640, 480);
@@ -151,6 +187,107 @@ public class App extends Application {
           }  
         });
         
+        buttonRemove.setOnAction(new EventHandler<ActionEvent>(){
+        @Override
+          public void handle(ActionEvent t){
+              try{
+                  host = InetAddress.getLocalHost();
+              }
+              catch(UnknownHostException e){
+                  System.out.println("Host ID not found!");
+                  System.exit(1);
+              }
+              
+              labelDelete = new Label ("Please enter the details of the class you wish to remove.");
+              moduleCode = new TextField("Module code (eg. CS4076)");
+              dayOfWeek = new ChoiceBox();
+              dayOfWeek.getItems().add("Monday");
+              dayOfWeek.getItems().add("Tuesday");
+              dayOfWeek.getItems().add("Wednesday");
+              dayOfWeek.getItems().add("Thursday");
+              dayOfWeek.getItems().add("Friday");
+              startTime = new ChoiceBox();
+              for (int i = 9; i<18; i++){
+                  startTime.getItems().add(i+":00");
+              }
+              endTime = new ChoiceBox();
+              for (int i = 10; i<19; i++){
+                  endTime.getItems().add(i+":00");
+              }
+
+              roomCode = new TextField("Room Code (eg. CSG001)");
+              confirmRemove = new Button("Remove Class");
+              
+              Button buttonHome = new Button ("Back to home");
+              buttonHome.setOnAction(new EventHandler<ActionEvent> (){
+                            
+                @Override
+                public void handle(ActionEvent t){
+                    stage.setScene(scene);
+                }
+                
+            });
+             
+            
+            confirmRemove.setOnAction(new EventHandler<ActionEvent> () {
+               
+               @Override
+               public void handle(ActionEvent t){
+                    try 
+                    {
+                        Socket link = new Socket(host,PORT);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+                        PrintWriter out = new PrintWriter(link.getOutputStream(),true);
+                        
+                        
+                        String response= null;
+                        
+                        String[] startTimeSplit = startTime.getValue().toString().split(":");
+                        String[] endTimeSplit = endTime.getValue().toString().split(":");
+                        String message = "1/"+ moduleCode.getText()+"/"+dayOfWeek.getValue()+"/"+startTimeSplit[0]+"/"+endTimeSplit[0]+"/"+roomCode.getText();
+                        out.println(message);
+                        response = in.readLine();
+                        label.setText(response);
+                        
+                        
+                        
+                        
+                        try
+                        {
+                            System.out.println("\n* Closing connection... *");
+                            link.close();
+                        }catch(IOException e)
+                        {
+                            System.out.println("Unable to disconnect/close!");
+                            System.exit(1);
+                        }
+                    }catch(IOException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                    finally
+                    {
+                        
+                    }
+                    Button buttonRemoveHome = new Button ("Back to home");
+            buttonRemoveHome.setOnAction(new EventHandler<ActionEvent> (){
+                            
+                @Override
+                public void handle(ActionEvent t){
+                    stage.setScene(scene);
+                }
+            });
+                    VBox box = new VBox(label, buttonRemoveHome);
+               var sceneConfirmRemove = new Scene(box, 640, 480);
+               stage.setScene (sceneConfirmRemove);
+               }
+               
+           });
+            VBox box = new VBox(labelDelete, moduleCode, dayOfWeek, startTime, endTime, roomCode,confirmRemove, buttonHome);
+            var sceneRemove = new Scene(box, 640, 480);
+            stage.setScene(sceneRemove);
+          }  
+        });
         
         buttonDisplay.setOnAction(new EventHandler<ActionEvent>() {
         @Override
@@ -167,13 +304,12 @@ public class App extends Application {
            
            //Opening a new window for the selected action
            //Box with the necessary information
-           labelDisplay = new Label ("From what class would you like to see the schedule ?"); 
+           titleDisplay = new Label("Display schedule");
+           labelDisplay = new Label ("ENTER CLASS CODE"); 
            textFieldRoomDisplay = new TextField("");
            confirmDisplay = new Button ("Show schedule");
            
            //Button back to home
-                        
-            Button buttonHome = new Button ("Back to home");
             buttonHome.setOnAction(new EventHandler<ActionEvent> (){
                             
                 @Override
@@ -294,8 +430,6 @@ public class App extends Application {
 
                                 
                                 //Button back to home
-                        
-                                Button buttonHome = new Button ("Back to home");
                                 buttonHome.setOnAction(new EventHandler<ActionEvent> (){
 
                                     @Override
@@ -303,11 +437,14 @@ public class App extends Application {
                                             stage.setScene(scene);
                                         }
                                 });
+                               
 
                                 // Setting the scene
                                 VBox boxSchedule = new VBox();
+                                HBox homeButtonContainer = new HBox(buttonHome);
+                                homeButtonContainer.setAlignment(Pos.CENTER);
                                 // Add GridPane and Button to the VBox
-                                boxSchedule.getChildren().addAll(gridPane, buttonHome);
+                                boxSchedule.getChildren().addAll(gridPane, homeButtonContainer);
                                 // Set the VBox to stretch the GridPane vertically to take up all available space
                                 VBox.setVgrow(gridPane, Priority.ALWAYS);
                                 
@@ -340,20 +477,102 @@ public class App extends Application {
                }
            });
            
-                              
-            VBox box = new VBox(labelDisplay, textFieldRoomDisplay, confirmDisplay, label, buttonHome);
-            var sceneDisplay = new Scene(box, 640, 480);
+            GridPane content = new GridPane();
+            
+            // Set the GridPane to expand and fill the available space, equalizing column widths
+            ColumnConstraints columnConstraintsContent = new ColumnConstraints();
+            columnConstraintsContent.setPercentWidth(100.0);
+            content.getColumnConstraints().addAll(columnConstraintsContent);
+                                
+            //Row constraints
+            RowConstraints rowConstraintsContent = new RowConstraints();
+            rowConstraintsContent.setPercentHeight(80.0 / 6);
+                                
+            content.getRowConstraints().addAll(rowConstraintsContent, rowConstraintsContent,
+                    rowConstraintsContent, rowConstraintsContent, rowConstraintsContent, rowConstraintsContent);
+            
+            content.setPadding(new Insets(10, 10, 10, 10));
+            
+            titleDisplay.setStyle("-fx-text-fill: #002633; -fx-font-weight: bold; "
+                    + "-fx-font-size: 24px");
+            
+            content.add(titleDisplay, 0, 0);
+            
+            GridPane.setHalignment(titleDisplay, HPos.CENTER);
+            GridPane.setValignment(titleDisplay, VPos.CENTER);
+            
+            content.add(labelDisplay, 0, 1);
+            GridPane.setHalignment(labelDisplay, HPos.CENTER);
+            GridPane.setValignment(labelDisplay, VPos.CENTER);
+            
+            content.add(textFieldRoomDisplay, 0, 2);
+            GridPane.setHalignment(textFieldRoomDisplay, HPos.CENTER);
+            GridPane.setValignment(textFieldRoomDisplay, VPos.CENTER);
+            
+            confirmDisplay.setStyle("-fx-text-fill: #002633; -fx-border-color: #002633");
+            
+            content.add(confirmDisplay, 0, 4);
+            GridPane.setHalignment(confirmDisplay, HPos.CENTER);
+            GridPane.setValignment(confirmDisplay, VPos.CENTER);
+            
+            label.setStyle("-fx-font-style: italic");
+            
+            content.add(label, 0, 6);
+            GridPane.setHalignment(label, HPos.CENTER);
+            GridPane.setValignment(label, VPos.CENTER);
+            
+            BorderPane borderPane = new BorderPane();
+            
+            borderPane.setPadding(new Insets(20, 100, 20, 100));
+            
+            borderPane.setCenter(content);
+            
+            HBox homeButtonContainer = new HBox(buttonHome);
+            homeButtonContainer.setAlignment(Pos.CENTER);
+            borderPane.setBottom(homeButtonContainer);
+            
+            
+            var sceneDisplay = new Scene(borderPane, 640, 480);
             stage.setScene(sceneDisplay);
            
 
            
         }});
         
+        buttonQuit.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                try{
+                    try{
+                        host = InetAddress.getLocalHost();
+                    }catch(UnknownHostException e){
+                        System.out.println("Host ID not found!");
+                        System.exit(1);
+                    }
+                    Socket link = new Socket(host,PORT);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+                    PrintWriter out = new PrintWriter(link.getOutputStream(),true);
+                    String response= null;
+                    out.println("3");
+                    response = in.readLine();
+                    if(response.equals("TERMINATE")){
+                        try{
+                            link.close();
+                            stage.close();
+                        }catch(IOException ex){
+                            System.exit(1);
+                        }                          
+                    }                      
+                }catch(IOException ex){
+                    ex.printStackTrace();
+                }
+            }       
+        });
+        
         
         stage.setScene(scene);
         stage.show();
     }
-    
 
     public static void main(String[] args) {
         launch();
--- TCPEchoServer/src/main/java/com/mycompany/tcpechoserver/TCPEchoServer.java
+++ TCPEchoServer/src/main/java/com/mycompany/tcpechoserver/TCPEchoServer.java
@@ -18,10 +18,12 @@ public class TCPEchoServer {
   private static final int PORT = 1234;
   private static int clientConnections = 0;
   private static ArrayList<Classes> classes;
+  private static ArrayList<String> moduleCodes;
 
   public static void main(String[] args) {
     System.out.println("Opening port...\n");
     classes = new ArrayList();
+    moduleCodes = new ArrayList();
     try 
     {
         servSock = new ServerSocket(PORT);      //Step 1.
@@ -48,6 +50,7 @@ public class TCPEchoServer {
       clientConnections++;
       BufferedReader in = new BufferedReader( new InputStreamReader(link.getInputStream())); //Step 3.
       PrintWriter out = new PrintWriter(link.getOutputStream(),true); //Step 3.
+      String message = "";
       
       String messageClient = in.readLine();         //Step 4.
       System.out.println("Message received from client: " + clientConnections + "  "+ messageClient);
@@ -66,12 +69,115 @@ public class TCPEchoServer {
           case "2":
               action = "consult";
               break;
+          case "3":
+              action = "quit";
+              break;
           default:
               action = null;
-              throw new IncorrectActionException();
+              throw new IncorrectActionException("No action selected.");
       }
+     
       
       //Depending on the action coming from the client, do the thing
+      if(action.equals("add")){
+        try{
+              int day;
+            switch (actionClient[2]) {
+                case "Monday":
+                    day = 1;
+                    break;
+                case "Tuesday":
+                    day = 2;
+                    break;
+                case "Wednesday":
+                    day = 3;
+                    break;
+                case "Thursday":
+                    day = 4;
+                    break;
+                default:
+                    day = 5;
+                    break;
+            }
+          
+            Classes scheduledClass = new Classes(actionClient[1], day, Integer.parseInt(actionClient[3]), Integer.parseInt(actionClient[4]), actionClient[5]);
+
+            if(Integer.parseInt(actionClient[4])<=Integer.parseInt(actionClient[3])){
+                throw new IncorrectActionException("The class end time must be after the start time. Class not scheduled.");
+            }
+            
+            if(classes.isEmpty()==false){
+                if(moduleCodes.contains(scheduledClass.getClassCode())==false && moduleCodes.size()>=5){
+                    throw new IncorrectActionException("A schedule cannot contain more than 5 modules per semester.");
+                }
+                for (int i = 0; i<classes.size(); i++){
+                    if(scheduledClass.getDay()==classes.get(i).getDay()){
+                        int newStartTime = scheduledClass.getStartingTime();
+                        int newEndTime = scheduledClass.getEndingTime();
+                        int oldStartTime = classes.get(i).getStartingTime();
+                        int oldEndTime = classes.get(i).getEndingTime();
+                        if(newStartTime==oldStartTime
+                            ||newEndTime==oldEndTime
+                            ||(newStartTime>oldStartTime&&newStartTime<oldEndTime)
+                            ||(newEndTime>oldStartTime&&newEndTime<oldEndTime)){
+                                throw new IncorrectActionException("The requested class clashes with an existing class. Class not scheduled.");
+                        }
+                    }                                             
+                }
+                classes.add(scheduledClass);
+                message = "The class was added to the schedule successfully."; 
+            }else{
+                classes.add(scheduledClass);
+                moduleCodes.add(scheduledClass.getClassCode());
+                message = "The class was added to the schedule successfully.";
+            }
+        }catch (IncorrectActionException e){
+            message = e.getIncorrectActionException();
+          
+        }  
+      }
+      
+      if(action.equals("remove")){
+          try{
+              int day;
+            switch (actionClient[2]) {
+                case "Monday":
+                    day = 1;
+                    break;
+                case "Tuesday":
+                    day = 2;
+                    break;
+                case "Wednesday":
+                    day = 3;
+                    break;
+                case "Thursday":
+                    day = 4;
+                    break;
+                default:
+                    day = 5;
+                    break;
+            }
+            Classes scheduledClass = new Classes(actionClient[1], day, Integer.parseInt(actionClient[3]), Integer.parseInt(actionClient[4]), actionClient[5]);
+            if(classes.isEmpty()){
+                throw new IncorrectActionException("There are currently no scheduled classes to be removed");
+            }
+            for(int i = 0; i<classes.size(); i++){
+                if(scheduledClass.getClassCode().equals(classes.get(i).getClassCode())
+                        &&scheduledClass.getDay()==classes.get(i).getDay()
+                        &&scheduledClass.getStartingTime()==classes.get(i).getStartingTime()
+                        &&scheduledClass.getEndingTime()==classes.get(i).getEndingTime()
+                        &&scheduledClass.getRoom().equals(classes.get(i).getRoom())){
+                    classes.remove(i);
+                    message = "Class removed successfully!";
+                }else{
+                    throw new IncorrectActionException("There is currently no scheduled class that fits the given details.");
+                }
+            }
+          }catch(IncorrectActionException e){
+              message = e.getIncorrectActionException();
+          }
+      }
+      
       if(action == "consult"){
           
           //TEST
@@ -106,7 +212,19 @@ public class TCPEchoServer {
           }
       }
       
-      //out.println("Response from Server (Capitalized Message): " + message.toUpperCase());     //Step 4.
+      if(action.equals("quit")){
+          try {
+            out.println("TERMINATE");
+            link.close();				    //Step 5.
+	}
+       catch(IOException e)
+       {
+            System.out.println("Unable to disconnect!");
+	    System.exit(1);
+       }
+      }
+      
+      out.println(message);     //Step 4.
      }
     catch(IOException e)
     {
@@ -116,17 +234,6 @@ public class TCPEchoServer {
     {
         System.out.println(e.getIncorrectActionException());
     }
-    finally 
-    {
-       try {
-	    System.out.println("\n* Closing connection... *");
-            link.close();				    //Step 5.
-	}
-       catch(IOException e)
-       {
-            System.out.println("Unable to disconnect!");
-	    System.exit(1);
-       }
-    }
+    
   } // finish run method 
 } // finish the class
